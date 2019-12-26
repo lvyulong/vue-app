f13ee37d98b8855e676c3e4cb4e3e87155edfc5f
吕玉龙 <lvyl@hrtps.com>
2019-12-26 18:14:36 +0800

    fixed

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ window.global_data = {
     //除非需要写死域名的地方才会用到，否则需用location.origin动态获取
     domain: {
         dev: 'http://{{domain}}',
-        prod: '{{prod}}',
+        prod: '',
     },
     devIp: '{{ip}}',
 };
